aab11d6460c84a2e3e960f393f9550ed5bf0e943
Julien Ripouteau <julien@ripouteau.com>
2025-04-23 22:29:57 +0200

    feat: add get method

--- src/object.ts
+++ src/object.ts
@@ -164,3 +164,42 @@ export function omitBy<T extends Record<string, any>>(
 
   return result
 }
+
+
+/**
+ * Dynamically get a nested value from an array or object with a
+ * string.
+ *
+ * @example
+ * ```ts
+ * const person = {
+ *   name: 'John',
+ *   friends: [{ name: 'Jane' }]
+ * }
+ *
+ * get(person, 'friends[0].name')
+ * // => 'Jane'
+ * ```
+ */
+export function get<TDefault = unknown>(
+  value: any,
+  path: string,
+  defaultValue?: TDefault,
+): TDefault {
+  const segments = path.split(/[\.\[\]]/g)
+  let current: any = value
+
+  for (const key of segments) {
+    if (current === null) return defaultValue as TDefault
+    if (current === undefined) return defaultValue as TDefault
+
+    const unquotedKey = key.replace(/['"]/g, '')
+    if (unquotedKey.trim() === '') continue
+
+    current = current[unquotedKey]
+  }
+
+  if (current === undefined) return defaultValue as TDefault
+
+  return current
+}
